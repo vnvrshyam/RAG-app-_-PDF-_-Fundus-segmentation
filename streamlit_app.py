5a723d95c598b213f76c6ef226347a088f7ac5b8
Valluri Naga Venkata Ratna Shyam <108254544+vnvrshyam@users.noreply.github.com>
2024-09-24 20:01:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit_app as st
+import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
